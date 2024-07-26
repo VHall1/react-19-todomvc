142339bf3f5eb675e33a035ae47028a3ecb449db
VHall1 <victorhallpsn@hotmail.com>
2024-07-26 16:45:49 +0100

    more layout stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,68 @@
-export default function Home() {
+import { db } from "@/util/db";
+
+export default async function Home() {
+  const todos = await db.todo.findMany();
+
+  const handleFormSubmit = async (formData: FormData) => {
+    "use server";
+    const content = formData.get("content")?.toString();
+    if (!content) return;
+    await db.todo.create({ data: { content } });
+  };
+
   return (
     <>
       <section className="todoapp">
         <header className="header">
           <h1>todos</h1>
-          <div className="input-container">
+          <form action={handleFormSubmit} className="input-container">
             <input
               id="todo-input"
+              name="content"
               className="new-todo"
               placeholder="What needs to be done?"
             />
             <label htmlFor="todo-input" className="visually-hidden">
               New Todo Input
             </label>
-          </div>
+          </form>
         </header>
-        <main className="main"></main>
+        <main className="main">
+          <div className="toggle-all-container">
+            <input id="toggle-all" className="toggle-all" type="checkbox" />
+            <label htmlFor="toggle-all" className="toggle-all-label">
+              Toggle All Input
+            </label>
+          </div>
+          <ul className="todo-list">
+            {todos.map((todo) => (
+              <div className="view" key={todo.id}>
+                <li className="">
+                  <input className="toggle" type="checkbox" />
+                  <label>{todo.content}</label>
+                  <button className="destroy"></button>
+                </li>
+              </div>
+            ))}
+          </ul>
+        </main>
+        <footer className="footer">
+          <span className="todo-count">1 item left!</span>
+          <ul className="filters">
+            <li>
+              <a className="selected">All</a>
+            </li>
+            <li>
+              <a>Active</a>
+            </li>
+            <li>
+              <a>Completed</a>
+            </li>
+          </ul>
+          <button className="clear-completed" disabled>
+            Clear completed
+          </button>
+        </footer>
       </section>
       <footer className="info">
         <p>Double-click to edit a todo</p>
